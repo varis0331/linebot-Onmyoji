5288e426ac0df3d092cfacf22d2f482569b90da0
varis0331 <65707840+varis0331@users.noreply.github.com>
2022-06-01 12:22:07 +0800

    ok

--- app.py
+++ app.py
@@ -177,8 +177,8 @@ def handle_message(event):
 #=====活動更新專區=====
 
     elif '活動' == msg  in msg :
-        line_bot_api.reply_message(event.reply_token,  [ImageSendMessage(original_content_url='',preview_image_url=''), 
-                                                        ImageSendMessage(original_content_url='',preview_image_url='')])
+        line_bot_api.reply_message(event.reply_token,  [ImageSendMessage(original_content_url='',preview_image_url='https://i.imgur.com/BNE4TQA.jpg'), 
+                                                        ImageSendMessage(original_content_url='',preview_image_url='https://i.imgur.com/BNE4TQA.jpg')])
 
 #=====逢魔攻略專區=====
 
